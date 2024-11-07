52818756e7ac9179cab371dd93bbd5f5ea418aa6
Walt Mayfield <waltmayf@amazon.com>
2024-11-07 11:37:54 -0600

    Prompt tuning

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -20,6 +20,7 @@ export const productionAgentFunction = defineFunction({
   environment: {
     // MODEL_ID: 'anthropic.claude-3-5-sonnet-20240620-v1:0'
     MODEL_ID: 'anthropic.claude-3-sonnet-20240229-v1:0'
+    // MODEL_ID: 'anthropic.claude-3-haiku-20240307-v1:0'
   },
   runtime: 20
 });
