e445ba6d1c8e9aff699c44ea2b4a04680f23420a
Walt Mayfield <waltmayf@amazon.com>
2024-11-13 09:08:23 -0600

    initial commit

--- src/components/chat-ui/chat-ui-message.tsx
+++ src/components/chat-ui/chat-ui-message.tsx
@@ -566,39 +566,6 @@ export default function ChatUIMessage(props: ChatUIMessageProps) {
                       props.message.content
                   }
                 </pre>/* Render as YAML */;
-              // case 'tool_table':
-              //   return <ReactMarkdown
-              //     remarkPlugins={[remarkGfm]}
-              //     components={{
-              //       table: ({ ...props }) => (
-              //         <table className={styles.markdownTable} {...props} />
-              //       ),
-              //       tr: ({ ...props }) => {
-
-              //         //Get the value of the relevance score in each table row
-              //         const children = React.Children.toArray(props.children);
-
-              //         const relevanceScoreTd = children[children.length - 2]; // should be second from the last
-
-              //         if (!(React.isValidElement(relevanceScoreTd))) throw new Error("Invalid second from last <td> element");
-
-              //         const relevanceScoreTdValue = relevanceScoreTd?.props?.children || '10'; // Here you can impliment conditional hiding of rows
-
-              //         // console.log("relevanceScore <td> value:", relevanceScoreTdValue); // This will log the value
-
-              //         //Hide rows with a low relevanceScore
-              //         if (hideRows && parseInt(relevanceScoreTdValue) < 4) return <tr className={styles.hiddenRow} {...props} />
-
-              //         // Add a 📄 to the second from the last child in props
-              //         // children.splice(children.length - 2, 0, ' ���');
-              //         // children[children.length - 2].props?.children = 'hello'
-
-              //         else return <tr {...props} />
-              //       },
-              //     }}
-              //   >
-              //     {props.message.content}
-              //   </ReactMarkdown>;
               default: //Default will be to render markdown
                 return <ReactMarkdown
                   remarkPlugins={[remarkGfm]}
