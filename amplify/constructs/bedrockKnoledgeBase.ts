ed7d6c79c839816ca3ef1cb8939f02c62bd6a9d5
Walt Mayfield <waltmayf@amazon.com>
2024-11-14 15:50:21 -0600

    Updated model_ID for the production agent

--- amplify/constructs/bedrockKnoledgeBase.ts
+++ amplify/constructs/bedrockKnoledgeBase.ts
@@ -162,7 +162,7 @@ export class AuroraBedrockKnoledgeBase extends Construct {
     })
 
     this.knowledgeBase = new bedrock.CfnKnowledgeBase(this, "KnowledgeBase", {
-      name: `${id}-${rootStack.stackName.slice(-3)}`,
+      name: `${id}-${rootStack.stackName.slice(-4)}`,
       roleArn: knoledgeBaseRole.roleArn,
       description: 'This knowledge base stores sql table definitions',
       knowledgeBaseConfiguration: {
