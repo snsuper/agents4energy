1fd433b2c18cace650bfc133f8352e27c46e047a
Walt Mayfield <waltmayf@amazon.com>
2025-01-13 11:46:13 -0600

    Renamed maintenance kb to allow for replacement

--- amplify/agents/maintenance/maintenanceAgent.ts
+++ amplify/agents/maintenance/maintenanceAgent.ts
@@ -744,9 +744,9 @@ export function maintenanceAgentBuilder(scope: Construct, props: AgentProps) {
 
     // ===== MAINTENANCE KNOWLEDGE BASE =====
     // Bedrock KB with OpenSearchServerless (OSS) vector backend
-    const maintenanceKnowledgeBase = new cdkLabsBedrock.KnowledgeBase(scope, `MaintKB`, {//${stackName.slice(-5)}
+    const maintenanceKnowledgeBase = new cdkLabsBedrock.KnowledgeBase(scope, `MaintenanceKB`, {//${stackName.slice(-5)}
         embeddingsModel: cdkLabsBedrock.BedrockFoundationModel.TITAN_EMBED_TEXT_V2_1024,
-        name: knowledgeBaseName,
+        // name: knowledgeBaseName,
         instruction: `You are a helpful question answering assistant. You answer user questions factually and honestly related to industrial facility maintenance and operations`,
         description: 'Maintenance Knowledge Base',
     });
